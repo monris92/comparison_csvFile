0a8bda9108e8c41cd5f8e8bb1d1254b9bd13c19f
monris92 <42492566+monris92@users.noreply.github.com>
2024-02-15 16:09:44 +0800

    Update comparison_report.py

--- comparison_report.py
+++ comparison_report.py
@@ -97,8 +97,8 @@ def compare_csv(downloaded_csv, local_csv_file, validations):
 
 # Main function
 def main():
-    username = os.environ.get('USERNAME')
-    password = os.environ.get('PASSWORD')
+    username = os.environ('USERNAME')
+    password = os.environ('PASSWORD')
     local_csv_file = 'astana_tegal_gundul_people-template.csv'
 
     token = get_access_token(username, password)
