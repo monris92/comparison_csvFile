2b36f27211ed852dfc28e8942f5d952337877962
ahmad.faris1 <ahmad.faris@wartek.belajar.id>
2024-02-16 13:58:31 +0800

    017

--- reports.py
+++ reports.py
@@ -1,5 +1,8 @@
 import time
+
 import requests
+from pip._internal.utils import logging
+
 from config import *
 
 
@@ -29,6 +32,30 @@ def request_csv_generation(token):
         print("CSV generation request failed.")
         print(f"Status Code: {response.status_code}, Response: {response.content}")
         return None
+
+
+# Setting up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def get_latest_report_id(token):
+    status_url = f'{API_BASE_URL_V2}/reports/cemetery/{CEMETERY_NAME}/status/'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(status_url, headers=headers)
+    if response.status_code == 200:
+        reports = response.json()
+        if not reports:
+            logging.info("No reports found.")
+            return None
+        # Assuming reports are sorted newest first
+        latest_report = reports[0]  # Get the first report which should be the latest
+        report_id = latest_report.get('id')
+        logging.info(f"Latest report ID retrieved: {report_id}")
+        return report_id
+    else:
+        logging.error(f"Failed to retrieve reports. Status Code: {response.status_code}")
+        return None
+
+
 
 # Check the status of the CSV generation and download when ready
 def check_csv_status_and_download(token, report_id, max_retries=10, retry_delay=30):
@@ -64,6 +91,8 @@ def download_csv_report(file_url, report_id):
     else:
         print(f"Failed to download CSV. Status Code: {response.status_code}")
         return None
+# Use the get_latest_report_id function to get the latest report ID dynamically
+
 
 def delete_report(report_id, token):
     """Delete the CSV report from the server."""
