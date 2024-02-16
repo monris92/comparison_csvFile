cbe958cacc3c8912224884b5e5d3a14c6a019273
ahmad.faris1 <ahmad.faris@wartek.belajar.id>
2024-02-16 14:22:14 +0800

    021

--- reports.py
+++ reports.py
@@ -39,28 +39,19 @@ def request_csv_generation(token):
 def check_csv_status_and_download(token, report_id, max_retries=10, retry_delay=30):
     status_url = f'{API_BASE_URL_V2}/reports/cemetery/{CEMETERY_NAME}/status/'
     headers = {'Authorization': f'Bearer {token}'}
-    retries = 0
-    while retries < max_retries:
+    while True:
         response = requests.get(status_url, headers=headers)
         if response.status_code == 200:
-            report = next((r for r in response.json() if r.get('id') == report_id), None)
-            if report and report.get('status').lower() == "finished":
-                file_url = report.get('file')
-                print(f"Report {report_id} is finished. Downloading CSV file from: {file_url}")
-                logging.info(f"Checking and downloading from url: {status_url}")
-                return download_csv_report(file_url, report_id)
-            else:
-                print(f"Waiting for report to finish... (Attempt {retries+1}/{max_retries})")
-                time.sleep(retry_delay)
-                retries += 1
-                print(report_id)
-                logging.info(f"Checking and downloading report with ID: {report_id}")
+            reports = response.json()
+            for report in reports:
+                if report['status'].lower() == "finished":
+                    print(f"Report {report['id']} is finished. Downloading CSV file.")
+                    return report['file']
+            print("Waiting for report to finish...")
+            time.sleep(30)  # Wait before checking again
         else:
             print(f"Failed to check report status. Status Code: {response.status_code}")
             return None
-    print(f"Report status check has exceeded maximum retries.")
-    return None
-
 def download_csv_report(file_url, report_id):
     response = requests.get(file_url)
     if response.status_code == 200:
