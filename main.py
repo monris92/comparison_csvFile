6fb871d24a7dfcff3d6809d2c2eb9ebf09d33ea1
ahmad.faris1 <ahmad.faris@wartek.belajar.id>
2024-02-19 10:02:07 +0800

    026

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ import csv
 import sys
 import requests
 
-from utilities import get_access_token, request_csv_generation, check_csv_status_and_download, compare_csv, delete_report
-from config import USERNAME, PASSWORD, LOCAL_CSV_FILE
+from utilities import (
+    get_access_token,
+    request_csv_generation,
+    check_csv_status_and_download,
+    compare_csv,
+    delete_report,
+)
+from config import USERNAME, PASSWORD, LOCAL_CSV_FILE, DOWNLOAD_PATH
+
+def download_csv(csv_file_url, download_path):
+    response = requests.get(csv_file_url)
+    if response.status_code == 200:
+        with open(download_path, 'w', newline='', encoding='utf-8') as file:
+            file.write(response.content.decode('utf-8'))
+        return True
+    print(f"Failed to download CSV. Status Code: {response.status_code}")
+    return False
 
 def main():
     token = get_access_token(USERNAME, PASSWORD)
@@ -19,20 +34,16 @@ def main():
     if not csv_file_url:
         sys.exit(1)
 
-    response = requests.get(csv_file_url)
-    if response.status_code == 200:
-        decoded_content = response.content.decode('utf-8')
-        downloaded_csv = list(csv.reader(decoded_content.splitlines()))
-        validations = {(2, 0): 'A D 2',
-                       (3, 0): 'A D 6'}  # Replace with actual validations
-        if compare_csv(downloaded_csv, LOCAL_CSV_FILE, validations):
+    # Menyimpan CSV yang di-download ke path lokal
+    if download_csv(csv_file_url, DOWNLOAD_PATH):
+        validations = {(2, 0): 'A D 2', (3, 0): 'A D 6'}  # Replace with actual validations
+        if compare_csv(DOWNLOAD_PATH, LOCAL_CSV_FILE, validations):
             print("CSV validation successful.")
             delete_report(token, report_id)
         else:
             print("CSV validation failed.")
             sys.exit(1)
     else:
-        print(f"Failed to download CSV. Status Code: {response.status_code}")
         sys.exit(1)
 
 if __name__ == "__main__":
