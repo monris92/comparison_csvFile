82c4f3e0c566f90a1b293d857d188d997a117c29
ahmad.faris1 <ahmad.faris@wartek.belajar.id>
2024-02-21 15:21:29 +0800

    076

--- main.py
+++ main.py
@@ -40,7 +40,41 @@ def main():
         "cemeteries": []
     }
 
-    # interment_payload_cemetery = {}
+    interment_payload_cemetery = {
+        "attributes": [
+            "plot_id",
+            "date_of_birth",
+            "name",
+            "date_of_death",
+            "interment_date",
+            "interments_type",
+            "funeral_director",
+            "religion",
+            "funeral_minister",
+            "interment_number",
+            "next_of_kin",
+            "cremation_location",
+            "interment_depth",
+            "container_type",
+            "age",
+            "container_dimensions",
+            "returned_serviceman",
+            "applicant_relationship",
+            "occupation",
+            "next_of_kin_relationship",
+            "cause_of_death"
+        ],
+        "document_format": "csv",
+        "custom_fields": [
+            "astana_tegal_gundul_maiden_name",
+            "astana_tegal_gundul_baby_section",
+            "astana_tegal_gundul_military_branch"
+        ],
+        "sections": [],
+        "cemeteries": [CEMETERY_NAME],
+        "date_from": None,
+        "date_to": None
+    }
 
     interment_payload_section = {
         "attributes": [
@@ -79,15 +113,34 @@ def main():
     }
 
     people_payload_cemetery = {
-
-        "attributes": REPORT_ATTRS,
+        "attributes": [
+            "plot_id",
+            "person_name",
+            "contact_number",
+            "contact_address",
+            "postal_address",
+            "email",
+            "roi_certificate",
+            "roi_payment_date",
+            "interment_date",
+            "interment_type",
+            "interment_number",
+            "date_of_birth",
+            "date_of_death",
+            "age"
+        ],
         "document_format": "csv",
         "sections": [],
         "cemeteries": [CEMETERY_NAME],
-        "chapters": REPORT_CHAPTERS,
+        "chapters": [
+            "roi_holders",
+            "roi_applicants",
+            "interments",
+            "next_of_kins",
+            "interment_applicants"
+        ],
         "first_name": None,
         "last_name": None
-
     }
 
     people_payload_sections = {
@@ -121,7 +174,25 @@ def main():
         "last_name": None
     }
 
-    business_payload_cemetery = {}
+    business_payload_cemetery = {
+        "attributes": [
+            "business_name",
+            "person_name",
+            "contact_number",
+            "contact_address",
+            "postal_address",
+            "email"
+        ],
+        "document_format": "csv",
+        "sections": [],
+        "cemeteries": [CEMETERY_NAME],
+        "chapters": [
+            "funeral_ministers",
+            "funeral_directors",
+            "interment_applicants"
+        ],
+        "business_name": None
+    }
 
     business_payload_sections = {
         "attributes": [
@@ -141,6 +212,72 @@ def main():
             "interment_applicants"
         ],
         "business_name": None
+    }
+
+    ROI_payload_cemetery = {
+        "attributes": [
+            "plot_id",
+            "roi_holder_1",
+            "application_date",
+            "payment_date",
+            "contact_number",
+            "payment_status",
+            "contact_address",
+            "payment_amount",
+            "postal_address",
+            "right_type",
+            "email",
+            "roi_certificate",
+            "roi_holder_2",
+            "service_need",
+            "roi_holder_3",
+            "plot_status",
+            "expiry_date",
+            "term_of_right",
+            "roi_holder_2_3_contacts"
+        ],
+        "document_format": "csv",
+        "custom_fields": [
+            "astana_tegal_gundul_place_of_birth",
+            "astana_tegal_gundul_place_of_death"
+        ],
+        "sections": [],
+        "cemeteries": [CEMETERY_NAME],
+        "date_from": None,
+        "date_to": None
+    }
+
+    ROI_payload_sections = {
+        "attributes": [
+            "plot_id",
+            "roi_holder_1",
+            "application_date",
+            "payment_date",
+            "contact_number",
+            "payment_status",
+            "contact_address",
+            "payment_amount",
+            "postal_address",
+            "right_type",
+            "email",
+            "roi_certificate",
+            "roi_holder_2",
+            "service_need",
+            "roi_holder_3",
+            "plot_status",
+            "expiry_date",
+            "term_of_right",
+            "roi_holder_2_3_contacts"
+        ],
+        "document_format": "csv",
+        "custom_fields": [
+            "astana_tegal_gundul_place_of_birth",
+            "astana_tegal_gundul_place_of_death"
+        ],
+        "sections": REPORT_SECTIONS,
+        "cemeteries": [],
+        "date_from": None,
+        "date_to": None
     }
 
     events_payload = {
@@ -190,6 +327,9 @@ def main():
     # process_report(token, 'business_cemetery', business_payload_cemetery, 'business/')
     process_report(token, 'business_sections', business_payload_sections, 'business/')
 
+    # process_report(token, 'ROI_cemetery', ROI_payload_cemetery, 'roi/')
+    process_report(token, 'ROI_sections', ROI_payload_sections, 'roi/')
+
     process_report(token, 'events', events_payload, 'events/')
 
     # process_report(token, 'events', events_payload, 'events/')
@@ -197,11 +337,12 @@ def main():
     # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/inv_summary/ done
     # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/interments/ done
     # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/people/ done
-    # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/business/
+    # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/business/ done
+    # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/roi/
 
 
 # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/act_summary/
-# - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/roi/
+
 # - {{baseUrl-v2}}/reports/cemetery/{{cemetery-unique_name}}/generate/log_activity/
 
 if __name__ == "__main__":
